ef2fb91f22229c3a66bfe6f7eefddf94a580bd35
Your Name <you@example.com>
2022-09-18 12:13:02 +0530

    Selenium Project updates'

--- src/test/java/testcases/BaseClass.java
+++ src/test/java/testcases/BaseClass.java
@@ -1,7 +1,9 @@
 package testcases;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -9,51 +11,69 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
 public class BaseClass {
+	
+	WebDriver driver;
 	
 	XSSFWorkbook wbook;
 	XSSFSheet sheet;
 	
-	WebDriver driver;
+	ExtentReports report;
+	ExtentTest test;
 	
-	@BeforeSuite
+	
+	@BeforeTest
 	public void DataSetUp() throws IOException {
+		
+		
 		FileInputStream fis = new FileInputStream("exceldata.xlsx");
-        
-        wbook = new XSSFWorkbook(fis);
-        sheet = wbook.getSheet("Sheet1");
-
-
+		
+		this.wbook = new XSSFWorkbook(fis);
+		this.sheet = wbook.getSheet("Sheet1");
+		
+		report = new ExtentReports("ExtentReport.html");
+		
 	}
 	
-	@AfterSuite
-	public void DataClean() throws IOException{
+	@AfterTest
+	public void DataClean() throws IOException {
 		
 		wbook.close();
+		
+		report.flush();
+		report.close();
 	}
 	
 	@BeforeMethod
-	public void SetUp() {
+	public void SetUp(Method method) {
+		
+		test = report.startTest(method.getName());
 		
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        
-        driver = new ChromeDriver();
-        
-        driver.get("https://www.simplilearn.com/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS );	
+		
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.simplilearn.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+		
 	}
-
+	
+	
 	@AfterMethod
 	public void TearDown() {
 		
-		// step 7 - close the browser
-        driver.close();
+		report.endTest(test);
+		
+		//Step7: Close the browser
+		driver.close();
 	}
+
 }
